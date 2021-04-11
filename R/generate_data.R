272072a7ec2b10ad6e77b47b5d2f0928578a5d1b
Robert Gombos <32964032+RogertG@users.noreply.github.com>
2021-04-12 02:32:38 +0300

    Update generate_data.R

--- R/generate_data.R
+++ R/generate_data.R
@@ -20,7 +20,7 @@
 ### create dataframe with starting points and transformed x and y depending on a formula
 generate_data <- function(formula) {
   print("generate data")
-  df <- seq(from = -pi, to = pi, by = 0.01) %>%
+  df <- seq(from = -pi*2, to = pi*2, by = 0.01) %>%
     expand.grid(x_i = ., y_i = .) %>%
     dplyr::mutate(!!!formula)
   return(df)
